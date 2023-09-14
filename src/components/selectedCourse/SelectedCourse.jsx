0b995a7450bc23ef47b8287ac4baeed14bc1231c
Rakib mia <rbepari404@gmail.com>
2023-09-14 12:22:57 -0600

    finished UI

--- src/components/selectedCourse/SelectedCourse.jsx
+++ src/components/selectedCourse/SelectedCourse.jsx
@@ -4,12 +4,12 @@ const SelectedCourse = ({selectedCourseItems}) => {
   return (
     <div className='lg:w-1/4 md:w-1/4'>
       <div className='bg-white overflow-hidden px-6  rounded-xl'>
-          <h1 className='mt-6 mb-4 text-[20px] font-semibold text-center text-[#2F80ED]'>Credit Hour Remaining 7 hr</h1>
+          <h1 className='mt-6 mb-4 text-[18px] font-semibold text-center text-[#2F80ED]'>Credit Hour Remaining 7 hr</h1>
           <hr />
           <h1 className='text-xl font-bold my-3'>Course Name</h1>
           <ol className='px-4'>
               {
-                selectedCourseItems.map((item) => <li className='text-lg mb-1 list-decimal text-[#1c1b1b99]' key={item.id}>{item.title}</li>)
+                selectedCourseItems.map((item, index) => <li key={index} className='text-lg mb-1 list-decimal text-[#1c1b1b99]'>{item.title}</li>)
               }
           </ol>
           <hr className='mt-3' />
