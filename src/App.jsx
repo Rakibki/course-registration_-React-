6cf9b84911773d043c7a21762544386df04b5c9a
Rakib mia <rbepari404@gmail.com>
2023-09-15 00:36:23 -0600

    implement handle click and add card

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import Navber from './components/navber/Navber'
 import CourseContainer from './components/Course-container/CourseContainer'
 import SelectedCourse from './components/selectedCourse/SelectedCourse'
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const App = () => {
 
   const [course, setCourse] = useState([])
   const [selectedCourseItems, setSelectedCourseItem] = useState([])
+  const [totolCredit, setTotalCredit] = useState(0)
+  const [totalPrice, setTotalPrice] = useState(0)
 
   useEffect(() => {
     fetch('data.json')
@@ -14,8 +19,25 @@ const App = () => {
     .then((data) => setCourse(data))
   },[])
 
+
   const handleSelect = (item) => {
+    const exist = selectedCourseItems.find((course) => course.id === item.id);
+    if(exist) {
+        return toast.warn("you have already selected this course")
+    }
+    
+    let creditTemp = item.Credit;
+    selectedCourseItems.forEach((item) => creditTemp = creditTemp + item.Credit);
+    setTotalCredit(creditTemp)
+
     setSelectedCourseItem([...selectedCourseItems, item])
+    
+
+    let priceTemp = item.price;
+    selectedCourseItems.forEach((item) => priceTemp = priceTemp + item.price)
+    setTotalPrice(priceTemp)
+
+
   }
 
   return (
@@ -23,7 +45,19 @@ const App = () => {
       <Navber />
       <div className='md:flex gap-2 lg:flex px-8 md:px-6 lg:px-14'>
           <CourseContainer handleSelect={handleSelect} course={course} />
-          <SelectedCourse selectedCourseItems={selectedCourseItems} />
+          <SelectedCourse totalPrice={totalPrice} totolCredit={totolCredit} selectedCourseItems={selectedCourseItems} />
+      </div>
+      <div>
+        <ToastContainer position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"/>
       </div>
     </div>
   )
--- src/components/selectedCourse/SelectedCourse.jsx
+++ src/components/selectedCourse/SelectedCourse.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const SelectedCourse = ({selectedCourseItems}) => {
+const SelectedCourse = ({selectedCourseItems, totalPrice, totolCredit}) => {
   return (
     <div className='lg:w-1/4 md:w-1/4'>
       <div className='bg-white overflow-hidden px-6  rounded-xl'>
@@ -13,9 +13,9 @@ const SelectedCourse = ({selectedCourseItems}) => {
               }
           </ol>
           <hr className='mt-3' />
-          <h2 className='text-base font-medium my-4'>Total Credit Hour:</h2>
+          <h2 className='text-base font-medium my-4'>Total Credit Hour: {totolCredit}</h2>
           <hr />
-          <h2 className='text-base font-semibold my-3'>Total Price: </h2>
+          <h2 className='text-base font-semibold my-3'>Total Price: {totalPrice} USD</h2>
       </div>
     </div>
   )
